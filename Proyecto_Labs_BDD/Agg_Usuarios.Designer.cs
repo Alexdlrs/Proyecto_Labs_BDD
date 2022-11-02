91432799b1054eec961edb4a93a5af3e9369456e
Alexdlrs <115825291+Alexdlrs@users.noreply.github.com>
2022-11-02 16:38:30 -0700

    Add files via upload

--- Proyecto_Labs_BDD/Agg_Usuarios.Designer.cs
+++ Proyecto_Labs_BDD/Agg_Usuarios.Designer.cs
@@ -50,6 +50,7 @@
             this.dataGridView3 = new System.Windows.Forms.DataGridView();
             this.tabPage3 = new System.Windows.Forms.TabPage();
             this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
@@ -64,14 +65,16 @@
             this.tabControl1.Controls.Add(this.tabPage1);
             this.tabControl1.Controls.Add(this.tabPage2);
             this.tabControl1.Controls.Add(this.tabPage3);
-            this.tabControl1.Location = new System.Drawing.Point(13, 13);
+            this.tabControl1.Location = new System.Drawing.Point(10, 11);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(1060, 542);
+            this.tabControl1.Size = new System.Drawing.Size(795, 440);
             this.tabControl1.TabIndex = 4;
             // 
             // tabPage1
             // 
+            this.tabPage1.Controls.Add(this.button1);
             this.tabPage1.Controls.Add(this.btn_Home);
             this.tabPage1.Controls.Add(this.label5);
             this.tabPage1.Controls.Add(this.label4);
@@ -88,10 +91,11 @@
             this.tabPage1.Controls.Add(this.btn_Agregar);
             this.tabPage1.Controls.Add(this.dataGridView1);
             this.tabPage1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage1.Name = "tabPage1";
-            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage1.Size = new System.Drawing.Size(1052, 513);
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage1.Size = new System.Drawing.Size(787, 414);
             this.tabPage1.TabIndex = 0;
             this.tabPage1.Text = "Agregar usuarios";
             this.tabPage1.UseVisualStyleBackColor = true;
@@ -103,9 +107,10 @@
             this.btn_Home.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_Home.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Home.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_Home.Location = new System.Drawing.Point(951, 431);
+            this.btn_Home.Location = new System.Drawing.Point(713, 350);
+            this.btn_Home.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Home.Name = "btn_Home";
-            this.btn_Home.Size = new System.Drawing.Size(80, 76);
+            this.btn_Home.Size = new System.Drawing.Size(60, 62);
             this.btn_Home.TabIndex = 15;
             this.btn_Home.UseVisualStyleBackColor = true;
             this.btn_Home.Click += new System.EventHandler(this.btn_Home_Click);
@@ -114,9 +119,10 @@
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(910, 17);
+            this.label5.Location = new System.Drawing.Point(682, 14);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(130, 29);
+            this.label5.Size = new System.Drawing.Size(103, 24);
             this.label5.TabIndex = 14;
             this.label5.Text = "Encargado";
             // 
@@ -124,9 +130,10 @@
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(574, 17);
+            this.label4.Location = new System.Drawing.Point(430, 14);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(207, 29);
+            this.label4.Size = new System.Drawing.Size(162, 24);
             this.label4.TabIndex = 13;
             this.label4.Text = "Nombre completo";
             // 
@@ -134,9 +141,10 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(338, 17);
+            this.label3.Location = new System.Drawing.Point(254, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(94, 29);
+            this.label3.Size = new System.Drawing.Size(72, 24);
             this.label3.TabIndex = 12;
             this.label3.Text = "Carrera";
             // 
@@ -144,9 +152,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(148, 17);
+            this.label2.Location = new System.Drawing.Point(111, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(136, 29);
+            this.label2.Size = new System.Drawing.Size(106, 24);
             this.label2.TabIndex = 11;
             this.label2.Text = "Contraseña";
             // 
@@ -154,51 +163,57 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(7, 17);
+            this.label1.Location = new System.Drawing.Point(5, 14);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 29);
+            this.label1.Size = new System.Drawing.Size(27, 24);
             this.label1.TabIndex = 10;
             this.label1.Text = "ID";
             // 
             // tbx_Encargado
             // 
             this.tbx_Encargado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_Encargado.Location = new System.Drawing.Point(913, 56);
+            this.tbx_Encargado.Location = new System.Drawing.Point(685, 46);
+            this.tbx_Encargado.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_Encargado.Name = "tbx_Encargado";
-            this.tbx_Encargado.Size = new System.Drawing.Size(133, 30);
+            this.tbx_Encargado.Size = new System.Drawing.Size(101, 26);
             this.tbx_Encargado.TabIndex = 9;
             this.tbx_Encargado.Tag = "";
             // 
             // tbx_Nom
             // 
             this.tbx_Nom.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_Nom.Location = new System.Drawing.Point(577, 56);
+            this.tbx_Nom.Location = new System.Drawing.Point(433, 46);
+            this.tbx_Nom.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_Nom.Name = "tbx_Nom";
-            this.tbx_Nom.Size = new System.Drawing.Size(319, 30);
+            this.tbx_Nom.Size = new System.Drawing.Size(240, 26);
             this.tbx_Nom.TabIndex = 8;
             // 
             // tbx_Carrera
             // 
             this.tbx_Carrera.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_Carrera.Location = new System.Drawing.Point(341, 56);
+            this.tbx_Carrera.Location = new System.Drawing.Point(256, 46);
+            this.tbx_Carrera.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_Carrera.Name = "tbx_Carrera";
-            this.tbx_Carrera.Size = new System.Drawing.Size(211, 30);
+            this.tbx_Carrera.Size = new System.Drawing.Size(159, 26);
             this.tbx_Carrera.TabIndex = 7;
             // 
             // tbx_Contraseña
             // 
             this.tbx_Contraseña.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_Contraseña.Location = new System.Drawing.Point(151, 56);
+            this.tbx_Contraseña.Location = new System.Drawing.Point(113, 46);
+            this.tbx_Contraseña.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_Contraseña.Name = "tbx_Contraseña";
-            this.tbx_Contraseña.Size = new System.Drawing.Size(165, 30);
+            this.tbx_Contraseña.Size = new System.Drawing.Size(125, 26);
             this.tbx_Contraseña.TabIndex = 6;
             // 
             // tbx_ID
             // 
             this.tbx_ID.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tbx_ID.Location = new System.Drawing.Point(7, 56);
+            this.tbx_ID.Location = new System.Drawing.Point(5, 46);
+            this.tbx_ID.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tbx_ID.Name = "tbx_ID";
-            this.tbx_ID.Size = new System.Drawing.Size(123, 30);
+            this.tbx_ID.Size = new System.Drawing.Size(93, 26);
             this.tbx_ID.TabIndex = 5;
             // 
             // btn_Regresar
@@ -207,9 +222,10 @@
             this.btn_Regresar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_Regresar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Regresar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_Regresar.Location = new System.Drawing.Point(856, 431);
+            this.btn_Regresar.Location = new System.Drawing.Point(642, 350);
+            this.btn_Regresar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Regresar.Name = "btn_Regresar";
-            this.btn_Regresar.Size = new System.Drawing.Size(80, 76);
+            this.btn_Regresar.Size = new System.Drawing.Size(60, 62);
             this.btn_Regresar.TabIndex = 3;
             this.btn_Regresar.UseVisualStyleBackColor = true;
             this.btn_Regresar.Click += new System.EventHandler(this.btn_Regresar_Click);
@@ -220,9 +236,10 @@
             this.btn_Eliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_Eliminar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_Eliminar.Location = new System.Drawing.Point(760, 431);
+            this.btn_Eliminar.Location = new System.Drawing.Point(570, 350);
+            this.btn_Eliminar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Eliminar.Name = "btn_Eliminar";
-            this.btn_Eliminar.Size = new System.Drawing.Size(80, 76);
+            this.btn_Eliminar.Size = new System.Drawing.Size(60, 62);
             this.btn_Eliminar.TabIndex = 2;
             this.btn_Eliminar.UseVisualStyleBackColor = true;
             // 
@@ -232,30 +249,33 @@
             this.btn_Agregar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.btn_Agregar.Cursor = System.Windows.Forms.Cursors.Hand;
             this.btn_Agregar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
-            this.btn_Agregar.Location = new System.Drawing.Point(665, 431);
+            this.btn_Agregar.Location = new System.Drawing.Point(499, 350);
+            this.btn_Agregar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btn_Agregar.Name = "btn_Agregar";
-            this.btn_Agregar.Size = new System.Drawing.Size(80, 76);
+            this.btn_Agregar.Size = new System.Drawing.Size(60, 62);
             this.btn_Agregar.TabIndex = 1;
             this.btn_Agregar.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(7, 109);
+            this.dataGridView1.Location = new System.Drawing.Point(5, 89);
+            this.dataGridView1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.RowHeadersWidth = 51;
             this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(1039, 304);
+            this.dataGridView1.Size = new System.Drawing.Size(779, 247);
             this.dataGridView1.TabIndex = 0;
             // 
             // tabPage2
             // 
             this.tabPage2.Controls.Add(this.dataGridView3);
             this.tabPage2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(1052, 513);
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage2.Size = new System.Drawing.Size(787, 414);
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Prestado";
             this.tabPage2.UseVisualStyleBackColor = true;
@@ -263,20 +283,22 @@
             // dataGridView3
             // 
             this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView3.Location = new System.Drawing.Point(7, 100);
+            this.dataGridView3.Location = new System.Drawing.Point(5, 81);
+            this.dataGridView3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView3.Name = "dataGridView3";
             this.dataGridView3.RowHeadersWidth = 51;
             this.dataGridView3.RowTemplate.Height = 24;
-            this.dataGridView3.Size = new System.Drawing.Size(1039, 319);
+            this.dataGridView3.Size = new System.Drawing.Size(779, 259);
             this.dataGridView3.TabIndex = 0;
             // 
             // tabPage3
             // 
             this.tabPage3.Controls.Add(this.dataGridView2);
-            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.tabPage3.Name = "tabPage3";
-            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage3.Size = new System.Drawing.Size(1052, 513);
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.tabPage3.Size = new System.Drawing.Size(787, 414);
             this.tabPage3.TabIndex = 2;
             this.tabPage3.Text = "Inventario";
             this.tabPage3.UseVisualStyleBackColor = true;
@@ -284,21 +306,35 @@
             // dataGridView2
             // 
             this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView2.Location = new System.Drawing.Point(7, 82);
+            this.dataGridView2.Location = new System.Drawing.Point(5, 67);
+            this.dataGridView2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dataGridView2.Name = "dataGridView2";
             this.dataGridView2.RowHeadersWidth = 51;
             this.dataGridView2.RowTemplate.Height = 24;
-            this.dataGridView2.Size = new System.Drawing.Size(1039, 337);
+            this.dataGridView2.Size = new System.Drawing.Size(779, 274);
             this.dataGridView2.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(434, 349);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 62);
+            this.button1.TabIndex = 16;
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Agg_Usuarios
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.MidnightBlue;
-            this.ClientSize = new System.Drawing.Size(1085, 567);
+            this.ClientSize = new System.Drawing.Size(814, 461);
             this.Controls.Add(this.tabControl1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Agg_Usuarios";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -338,5 +374,6 @@
         private System.Windows.Forms.TabPage tabPage3;
         private System.Windows.Forms.DataGridView dataGridView2;
         private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.Button button1;
     }
 }
